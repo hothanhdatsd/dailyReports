fb52217c5320094ecae2ef3fbcfb31a1a775dd44
HoThanhDat <hothanhdatsd@gmail.com>
2023-11-02 09:07:00 +0700

    modify test case

--- week_3010_0511/CodeWar/genarate_string_from_number_0111.rb
+++ week_3010_0511/CodeWar/genarate_string_from_number_0111.rb
@@ -5,8 +5,8 @@ def generate_number(number_length, count_number)
 
   arr = []
   while arr.length < number_length
-    numbers = (1..100).to_a.sample(3)
-    operations = ['+', '-'].sample(2)
+    numbers = (1..100).to_a.sample(count_number)   
+    operations = ['+', '-'].sample(count_number-1)   
     expression = if count_number == 2
                    "#{numbers.first} #{operations.first} #{numbers.last}"
                  else
@@ -17,16 +17,16 @@ def generate_number(number_length, count_number)
   end
   arr
 end
-p generate_number(2,2)
-# require 'test/unit'
-# # class Test::Unit
-# class TestMethodExample < Test::Unit::TestCase
-#   include Test::Unit::Assertions
-#   def test_non_empty_strings
-#     assert_equal(3, generate_number(3, 3).size)
-#     assert_equal(0, generate_number(0, 2).size)
-#     assert_equal(0, generate_number(0, 3).size)
-#     assert_equal(1, generate_number(1, 2).size)
-#     assert_equal(1, generate_number(1, 3).size)
-#   end
-# end
+p generate_number(3, 3)
+require 'test/unit'
+# class Test::Unit
+class TestMethodExample < Test::Unit::TestCase
+  include Test::Unit::Assertions
+  def test_non_empty_strings
+    assert_equal(3, generate_number(3, 3).size)
+    assert_equal(0, generate_number(0, 2).size)
+    assert_equal(0, generate_number(0, 3).size)
+    assert_equal(1, generate_number(1, 2).size)
+    assert_equal(1, generate_number(1, 3).size)
+  end
+end
