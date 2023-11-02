acee9da2791a8070365f956e1ca918b6b4665442
HoThanhDat <hothanhdatsd@gmail.com>
2023-11-02 08:59:27 +0700

    modify test case

--- week_3010_0511/CodeWar/genarate_string_from_number_0111.rb
+++ week_3010_0511/CodeWar/genarate_string_from_number_0111.rb
@@ -1,34 +1,32 @@
 # frozen_string_literal: true
 
 def generate_number(number_length, count_number)
-  return -1 if count_number != 2 || count_number != 3
+  return -1 if count_number != 2 && count_number != 3
 
   arr = []
-  numbers = []
   while arr.length < number_length
     numbers = (1..100).to_a.sample(3)
-    operation1 = ['+', '-'].sample(1).join('')
-    operation2 = ['+', '-'].sample(1).join('')
+    operations = ['+', '-'].sample(2)
     expression = if count_number == 2
-                   "#{numbers.first} #{operation1} #{numbers.last}"
+                   "#{numbers.first} #{operations.first} #{numbers.last}"
                  else
-                   "#{numbers.first} #{operation1} #{numbers.last} #{operation2} #{numbers[1]}"
+                   "#{numbers.first} #{operations.first} #{numbers.last} #{operations.last} #{numbers[1]}"
                  end
     result = eval(expression).to_i
     arr << expression if result.positive? && result < 100
   end
   arr
 end
-
-require 'test/unit'
-# class Test::Unit
-class TestMethodExample < Test::Unit::TestCase
-  include Test::Unit::Assertions
-  def test_non_empty_strings
-    assert_equal(3, generate_number(3, 3).size)
-    assert_equal(0, generate_number(0, 2).size)
-    assert_equal(0, generate_number(0, 3).size)
-    assert_equal(1, generate_number(1, 2).size)
-    assert_equal(1, generate_number(1, 3).size)
-  end
-end
+p generate_number(2,2)
+# require 'test/unit'
+# # class Test::Unit
+# class TestMethodExample < Test::Unit::TestCase
+#   include Test::Unit::Assertions
+#   def test_non_empty_strings
+#     assert_equal(3, generate_number(3, 3).size)
+#     assert_equal(0, generate_number(0, 2).size)
+#     assert_equal(0, generate_number(0, 3).size)
+#     assert_equal(1, generate_number(1, 2).size)
+#     assert_equal(1, generate_number(1, 3).size)
+#   end
+# end
